56ad46283df4a0a4bbdc6b19dc1af3c4d4f8169c
Nicolás Salas V <nikosalas@gmail.com>
2020-04-14 11:34:21 -0400

    tmux: Add italics terminfo and compiler script

---
+++ generate-terminfo.sh
@@ -0,0 +1,3 @@
+tic -o $HOME/.terminfo $HOME/dotfiles/terminfo/tmux.terminfo
+tic -o $HOME/.terminfo $HOME/dotfiles/terminfo/tmux-256color.terminfo
+tic -o $HOME/.terminfo $HOME/dotfiles/terminfo/xterm-256color.terminfo
