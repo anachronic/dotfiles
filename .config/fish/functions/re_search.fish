3361dbb5723ba09c52447ab60e81c6c4c24d5855
Nicolás Salas <nicolas.salas@imfd.cl>
2020-07-20 23:50:33 -0400

    fish: improve re_search and man commands

--- .config/fish/functions/re_search.fish
+++ .config/fish/functions/re_search.fish
@@ -1,3 +1,5 @@
 function re_search
-	eval (history | fzf)
+	history -z | fzf --read0 --height=10 --exact --tiebreak=index | perl -pe 'chomp if eof' | read -lz result
+    and commandline -- $result
+    commandline -f repaint
 end
