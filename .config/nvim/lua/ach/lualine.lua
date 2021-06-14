afd39d486145e001b3fef7e289512230d4ee494a
Nicolás Salas <nicolas.salas@imfd.cl>
2021-06-14 11:54:04 -0400

    neovim: Always show relative path in statusline

--- .config/nvim/lua/ach/lualine.lua
+++ .config/nvim/lua/ach/lualine.lua
@@ -1,7 +1,24 @@
+local function relativepath()
+    local path = vim.fn.expand('%')
+
+    if path == '' then path = '[No Name]' end
+
+    if vim.bo.modified then
+        path = path .. '[+]'
+    elseif vim.bo.modifiable == false or vim.bo.readonly then
+        path = path .. '[RO]'
+    end
+
+    return path
+end
+
 require('lualine').setup{
     options = {
         theme = 'seoul256',
         section_separators = '',
         component_separators = '',
+    },
+    sections = {
+        lualine_c = {relativepath}
     }
 }
