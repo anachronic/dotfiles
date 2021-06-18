e6cc4cf20dd6b4f75402371ab2a9d8a381588ff4
Nicolás Salas <nicolas.salas@imfd.cl>
2021-06-17 22:41:21 -0400

    neovim: Use fzy sorter for anything in telescope

--- .config/nvim/after/plugin/telescope.nvim.lua
+++ .config/nvim/after/plugin/telescope.nvim.lua
@@ -41,7 +41,7 @@ require('telescope').setup{
     -- file_sorter =  require'telescope.sorters'.get_fuzzy_file,
     file_sorter =  require'telescope.sorters'.get_fzy_sorter,
     file_ignore_patterns = {},
-    generic_sorter =  require'telescope.sorters'.get_eneric_fuzzy_sorter,
+    generic_sorter =  require'telescope.sorters'.get_fzy_sorter,
     shorten_path = true,
     winblend = 0,
     width = 0.75,
