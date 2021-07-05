fa055e0b35c72f5f365df7d9bf5372931a1c28ab
Nicolás Salas <nicolas.salas@imfd.cl>
2021-07-04 22:10:14 -0400

    neovim: Disable tree window picker

--- .config/nvim/after/plugin/tree.lua
+++ .config/nvim/after/plugin/tree.lua
@@ -1,5 +1,7 @@
 local util = require('ach.util')
 local map = util.map
 
+vim.g.nvim_tree_disable_window_picker = 1
+
 map('n', '<A-1>', '<cmd>NvimTreeToggle<CR>')
 map('n', '<leader>j', '<cmd>NvimTreeFindFile<CR>')
