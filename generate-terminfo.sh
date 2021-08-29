26731a6e7f15bef68d79836382d5d87b1686457d
Nicolás Salas <nsalas@fastmail.com>
2021-08-29 01:21:47 -0400

    terminfo: Delete related files
    I remember adding these configs because I saw them in a wincent's video,
    but I've noticed that standout and undercurl work just fine in latest
    versions of tmux and kitty, so I'm getting rid of them. They also seem
    to conflict with ssh quite a bit

--- generate-terminfo.sh
+++
@@ -1,3 +0,0 @@
-tic -o $HOME/.terminfo $HOME/dotfiles/terminfo/tmux.terminfo
-tic -o $HOME/.terminfo $HOME/dotfiles/terminfo/tmux-256color.terminfo
-tic -o $HOME/.terminfo $HOME/dotfiles/terminfo/xterm-256color.terminfo
