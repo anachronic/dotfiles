82fce57cfc91db4cc0371c56e66f3cdb3098cc4f
Nicolás Salas <nsalas@fastmail.com>
2021-12-01 00:50:43 -0300

    neovim: Add treesitter-based endwise for ruby

--- common/link/.config/nvim/lua/ach/autopairs.lua
+++ common/link/.config/nvim/lua/ach/autopairs.lua
@@ -4,6 +4,7 @@ local cond = require('nvim-autopairs.conds')
 
 npairs.setup()
 
+npairs.add_rules(require('nvim-autopairs.rules.endwise-ruby'))
 npairs.add_rules({
     rule('[ ', ' '), -- the regular [] rule will add the missing paren
     rule('{ ', ' ')
