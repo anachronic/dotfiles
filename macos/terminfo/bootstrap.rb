a582ae188f4f5ec5a6bc9371b63472b1c49e03ec
Nicolás Salas <nsalas@fastmail.com>
2022-05-16 01:38:59 -0400

    terminfo: allow strikethrough text

--- macos/terminfo/bootstrap.rb
+++ macos/terminfo/bootstrap.rb
@@ -5,8 +5,7 @@ system "mkdir -p ~/.terminfo"
 
 # Then compile the new terminfo database
 Dir[File.join(__dir__, '*.terminfo')].each do |terminfo|
-  system "tic -o ~/.terminfo #{terminfo}"
+  system "tic -x -o ~/.terminfo #{terminfo}"
 end
 
-puts
 puts "terminfo installed"
