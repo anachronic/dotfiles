bf7cfedaf61923ea70a2fe492e68d77a4831a32e
Nicolás Salas <nsalas@fastmail.com>
2022-05-17 23:09:08 -0400

    neovim: use lua when setting formatoptions
    I was previously using ftplugin + vimscript. Changed it to neovim 0.7.0
    lua autocmd and I had to move it to after/plugin instead

--- common/link/.config/nvim/after/ftplugin.vim
+++
@@ -1,5 +0,0 @@
-" Needs to be here to stop neovim builtin ftplugin from messing with it
-
-" Stop auto commenting everywhere
-" needs autocmd because neovim builtins explicitly set formatoptions
-autocmd FileType * set formatoptions=jqlt
---
+++ common/link/.config/nvim/after/plugin/formatoptions.lua
@@ -0,0 +1,9 @@
+-- Stop auto commenting everywhere
+-- needs autocmd because neovim builtins explicitly set formatoptions
+
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = '*',
+    callback = function()
+        vim.bo.formatoptions = 'jqlt'
+    end
+})
