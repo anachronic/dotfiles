0bb719506abcfd58d9628894e3bb7ca01d1804ce
Nicolás Salas <nsalas@fastmail.com>
2022-05-18 02:13:51 -0400

    neovim: follow stylua guidelines

--- common/link/.config/nvim/lua/ach/lualine.lua
+++ common/link/.config/nvim/lua/ach/lualine.lua
@@ -1,24 +1,26 @@
 local function relativepath()
-    local path = vim.fn.expand('%')
+	local path = vim.fn.expand("%")
 
-    if path == '' then path = '[No Name]' end
+	if path == "" then
+		path = "[No Name]"
+	end
 
-    if vim.bo.modified then
-        path = path .. '[+]'
-    elseif vim.bo.modifiable == false or vim.bo.readonly then
-        path = path .. '[RO]'
-    end
+	if vim.bo.modified then
+		path = path .. "[+]"
+	elseif vim.bo.modifiable == false or vim.bo.readonly then
+		path = path .. "[RO]"
+	end
 
-    return path
+	return path
 end
 
-require('lualine').setup{
-    options = {
-        theme = 'seoul256',
-        section_separators = '',
-        component_separators = '',
-    },
-    sections = {
-        lualine_c = {relativepath}
-    }
-}
+require("lualine").setup({
+	options = {
+		theme = "seoul256",
+		section_separators = "",
+		component_separators = "",
+	},
+	sections = {
+		lualine_c = { relativepath },
+	},
+})
--- common/link/.config/nvim/lua/ach/nvimtree.lua
+++ common/link/.config/nvim/lua/ach/nvimtree.lua
@@ -1,17 +1,17 @@
-local util = require('ach.util')
+local util = require("ach.util")
 
-require 'nvim-tree'.setup {
-    git = {
-        enable = false
-    },
-    actions = {
-        open_file = {
-            window_picker = {
-                enable = false
-            }
-        }
-    }
-}
+require("nvim-tree").setup({
+	git = {
+		enable = false,
+	},
+	actions = {
+		open_file = {
+			window_picker = {
+				enable = false,
+			},
+		},
+	},
+})
 
-vim.keymap.set('n', '<A-1>', '<cmd>NvimTreeToggle<CR>')
-vim.keymap.set('n', '<leader>j', '<cmd>NvimTreeFindFile<CR>')
+vim.keymap.set("n", "<A-1>", "<cmd>NvimTreeToggle<CR>")
+vim.keymap.set("n", "<leader>j", "<cmd>NvimTreeFindFile<CR>")
