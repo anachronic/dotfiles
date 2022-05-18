f0c6b1db6a41e2621abfc8c9cbb37289131785e9
Nicolás Salas <nsalas@fastmail.com>
2022-05-18 01:03:00 -0400

    neovim: try out sumneko_lua
    Not 100% convinced. Can't hurt though, gd works

--- common/link/.config/nvim/after/plugin/nvim-lspconfig.lua
+++ common/link/.config/nvim/after/plugin/nvim-lspconfig.lua
@@ -99,6 +99,25 @@ lspconfig.solargraph.setup{
     on_attach = on_attach
 }
 
+-- brew install lua-language-server
+-- pacman -S lua-language-server
+lspconfig.sumneko_lua.setup {
+    on_attach = on_attach,
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = {'vim'},
+            },
+            workspace = {
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+            telemetry = {
+                enable = false,
+            },
+        },
+    },
+}
+
 -- npm i -g vuels
 lspconfig.vuels.setup{
     on_attach = on_attach,
