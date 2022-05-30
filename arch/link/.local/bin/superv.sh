d61319e78e7eb260de386140f8e1e227d3292ebe
Nicolás Salas <nsalas@fastmail.com>
2022-05-30 02:01:16 -0400

    bin: search for st, alacritty and kitty windows for super+v

--- arch/link/.local/bin/superv.sh
+++ arch/link/.local/bin/superv.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 ACTIVE_WINDOW=$(xdotool getactivewindow)
-KITTY_WINDOWS=$(xdotool search --class kitty)
+TERM_WINDOWS=$(xdotool search --class "(kitty|alacritty|st)")
 
 if [ -n "${ACTIVE_WINDOW}" ]; then
-  if [[ "${KITTY_WINDOWS[@]}" =~ "${ACTIVE_WINDOW}" ]]; then
-    xdotool key --delay 0 --clearmodifiers ctrl+shift+v
-  else
-    xdotool key --delay 0 --clearmodifiers ctrl+v
-  fi
+    if [[ "${TERM_WINDOWS[@]}" =~ "${ACTIVE_WINDOW}" ]]; then
+        xdotool key --delay 0 --clearmodifiers ctrl+shift+v
+    else
+        xdotool key --delay 0 --clearmodifiers ctrl+v
+    fi
 fi
