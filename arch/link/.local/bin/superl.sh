95a5daa78cf2d9e7afa0b3d31532ea96809c8d63
Nicolás Salas <nsalas@fastmail.com>
2022-05-30 01:59:23 -0400

    bin: search chromium AND firefox for super+l binding

--- arch/link/.local/bin/superl.sh
+++ arch/link/.local/bin/superl.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 ACTIVE_WINDOW=$(xdotool getactivewindow)
-CHROMIUM_WINDOWS=$(xdotool search --class chromium)
+BROWSER_WINDOWS=$(xdotool search --class "(chromium|firefox)")
 
 if [ -n "${ACTIVE_WINDOW}" ]; then
-  if [[ "${CHROMIUM_WINDOWS[@]}" =~ "${ACTIVE_WINDOW}" ]]; then
+  if [[ "${BROWSER_WINDOWS[@]}" =~ "${ACTIVE_WINDOW}" ]]; then
     xdotool key --delay 0 --clearmodifiers ctrl+l
   else
     xdotool key --delay 0 --clearmodifiers super+l
