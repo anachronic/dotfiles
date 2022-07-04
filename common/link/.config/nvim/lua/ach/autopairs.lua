fe13a61bdffb4cfa9831c5ec4064b8f16e14b357
Nicolás Salas <nsalas@fastmail.com>
2022-07-04 10:28:10 -0400

    neovim: temporarily use nvim-autopairs instead of smart-pairs
    smart-pairs breaks <CR> in cmp (very annoying), cannot be repeated with
    dot and is way too hungry when deleting, forcing extra spaces to be
    written and breaking my typing flow. I'll be going back to autopairs for
    the time being

--- common/link/.config/nvim/lua/ach/autopairs.lua
+++ common/link/.config/nvim/lua/ach/autopairs.lua
@@ -1,14 +1,18 @@
-require('pairs'):setup({
-    pairs = {
-        ['*'] = {
-            { '(', ')' },
-            { '[', ']' },
-            { '{', '}' },
-            { '\'', '\'', { ignore_pre = '\\w' } },
-            { '"', '"' },
-        },
-        rust = {
-            { '\'', '\'', { ignore_pre = '\\v(\\w|\\&)' } },
-        },
-    },
-})
+-- require('pairs'):setup({
+--     pairs = {
+--         ['*'] = {
+--             { '(', ')' },
+--             { '[', ']' },
+--             { '{', '}' },
+--             { '\'', '\'', { ignore_pre = '\\w' } },
+--             { '"', '"' },
+--         },
+--         rust = {
+--             { '\'', '\'', { ignore_pre = '\\v(\\w|\\&)' } },
+--         },
+--     },
+-- })
+
+local npairs = require('nvim-autopairs')
+
+npairs.setup()
