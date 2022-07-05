4b789c3584e306011ce78bec266e1086fcddcd8c
Nicolás Salas <nsalas@fastmail.com>
2022-07-05 00:00:59 -0400

    neovim: include black and isort in null-ls
    Both are applied (in the same order) when invoking format in python

--- common/link/.config/nvim/after/plugin/nvim-lspconfig.lua
+++ common/link/.config/nvim/after/plugin/nvim-lspconfig.lua
@@ -51,7 +51,8 @@ null_ls.setup({
     sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.gofmt,
-        null_ls.builtins.formatting.isort, -- not convinced, should be in a separate keystroke
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.isort,
         null_ls.builtins.formatting.prettier.with({
             prefer_local = 'node_modules/.bin',
         }),
