0fe79f5d90bb5a31f833bac26862a3f85e1c3540
Nicolás Salas <nsalas@fastmail.com>
2022-07-08 21:37:32 -0400

    dotfiles: lint sh files with shellcheck

---
+++ shcheck.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+lint() {
+    file=$1
+
+    if [ -f "$file" ]
+    then
+        shellcheck "$file"
+    else
+        for file in "$file"/*
+        do
+            [ -f "$file" ] && shellcheck "$file"
+        done
+    fi
+}
+
+lint "./arch/link/.local/bin"
+lint "./addplugin"
+lint "./rmplugin"
+lint "./shcheck.sh"
