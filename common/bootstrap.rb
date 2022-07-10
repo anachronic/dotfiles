147d310a33d304ec6257d358be5ce62cf3f98e3c
Nicolás Salas <nsalas@fastmail.com>
2022-07-10 03:20:46 -0400

    installer: delete remaining ruby files

--- common/bootstrap.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-# Assume ruby is installed
-# Assume GNU stow is also installed
-
-# Make ~/.config so that we don't stow EVERYTHING in .config
-# This can be particularly annoying in Linux
-system('mkdir -p ~/.config')
-system('mkdir -p ~/.local/bin')
-system('stow -v -R -t ~ -d ~/dotfiles/common/link .')
