7d0e38eb2bf2d3193a1a3c386dfe03fbd80b0bc9
Nicolás Salas <nsalas@fastmail.com>
2022-09-25 00:12:47 -0300

    neovim: use cnoreabbrev for fkups

--- common/link/.config/nvim/lua/ach/fkups.lua
+++ common/link/.config/nvim/lua/ach/fkups.lua
@@ -1,6 +1,14 @@
 -- This file is to create all commands i screw up everytime
 
-local command = vim.api.nvim_create_user_command
+local alias = function(lhs, rhs)
+    vim.cmd('cnoreabbrev ' .. lhs .. ' ' .. rhs)
+end
 
-command('Wq', 'wq', {})
-command('Q', 'quit', {})
+-- caps fkups
+alias('Wq', 'wq')
+alias('Q', 'quit')
+alias('Qa', 'qa')
+
+-- annoying writings
+alias('T', 'Telescope')
+alias('gs', 'Telescope git_commits')
