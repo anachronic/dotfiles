119054410f068e16c538129745b32d529a6e57a2
Nicolás Salas <nsalas@fastmail.com>
2022-09-25 02:20:05 -0300

    neovim: integrate diffview <> neogit

--- common/link/.config/nvim/after/plugin/neogit.lua
+++ common/link/.config/nvim/after/plugin/neogit.lua
@@ -1,5 +1,8 @@
 require('neogit').setup({
     disable_commit_confirmation = true,
+    integrations = {
+        diffview = true,
+    },
 })
 
 vim.keymap.set('n', '<leader>gg', '<cmd>Neogit<CR>')
