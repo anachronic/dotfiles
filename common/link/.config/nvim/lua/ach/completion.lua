2a5bd9196199cdb0a33cb253cee85a1d55a94d10
Nicolás Salas <nsalas@fastmail.com>
2022-10-11 00:11:25 -0300

    neovim: tweak cmp behavior
    This deserves an explanation.
    
    So I was using CR to accept a completion menu, regardless of the
    existence of the selection itself. That meant that if nothing was
    selected and I pressed CR, the completion would be accepted. It turns
    out that's ok in 90% of cases despite being a little invasive. No
    biggie, except that some sources are rather aggressive with completion,
    like sumneko_lua, which try to populate with whatever they can, leading
    to unwanted text being inserted.
    
    I also commented about this issue in my latest tag:
    https://github.com/anachronic/dotfiles/releases/tag/220924-working
    
    So I set out to settle this behavior:
    
    - CR will only accept if a completion is selected and will do whatever
    it does if nothing is selected
    - TAB will select next as usual, but if there's only one choice, it will
    accept it and close the completion menu
    - C-y will take the old CR role, meaning that it will eagerly accept
    either the selected completion, or the first in the menu
    
    There's a little downside to that, which is that Down will also accept
    if there's only one choice. I don't use Down very often, so that
    shouldn't be a problem. I guess I'm really lazy when it comes to
    tweaking these things. W/e, if I need it I'll change it
    
    This commit also replaces an `unpack` call with a `table.unpack`, which
    is supposed to be how lua 5.4 expects me to use it

--- common/link/.config/nvim/lua/ach/completion.lua
+++ common/link/.config/nvim/lua/ach/completion.lua
@@ -2,7 +2,7 @@ local cmp = require('cmp')
 local snippy = require('snippy')
 
 local has_words_before = function()
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
     return col ~= 0
         and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s')
             == nil
@@ -12,7 +12,11 @@ local select_next = cmp.mapping(function(fallback)
     if snippy.can_expand_or_advance() then
         snippy.expand_or_advance()
     elseif cmp.visible() then
-        cmp.select_next_item()
+        if #cmp.get_entries() == 1 then
+            cmp.confirm({ select = true })
+        else
+            cmp.select_next_item()
+        end
     elseif has_words_before() then
         cmp.complete()
     else
@@ -28,6 +32,22 @@ local select_previous = cmp.mapping(function()
     end
 end, { 'i', 's' })
 
+local accept_regardless = cmp.mapping(function (fallback)
+    if cmp.visible() then
+        cmp.confirm({ select = true })
+    else
+        fallback()
+    end
+end, { 'i', 's' })
+
+local accept_if_selected = cmp.mapping(function (fallback)
+    if not cmp.get_selected_entry() then
+        fallback()
+    else
+        cmp.confirm()
+    end
+end, { 'i', 's' })
+
 cmp.setup({
     -- don't preselect anything for me
     preselect = cmp.PreselectMode.None,
@@ -38,18 +58,14 @@ cmp.setup({
         end,
     },
     mapping = {
-        ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-        ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
         ['<Tab>'] = select_next,
         ['<S-Tab>'] = select_previous,
-
-        -- ['<C-j>'] = select_next,
-        -- ['<C-k>'] = select_previous,
 
         ['<down>'] = select_next,
         ['<up>'] = select_previous,
 
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-y>'] = accept_regardless,
+        ['<CR>'] = accept_if_selected,
     },
     sources = cmp.config.sources({
         { name = 'nvim_lsp' },
