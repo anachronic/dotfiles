2dd144b499c76503b65840449be54305874f0a63
Nicolás Salas <nsalas@fastmail.com>
2022-11-17 17:03:29 -0300

    neovim: set ignorecase

--- common/link/.config/nvim/init.lua
+++ common/link/.config/nvim/init.lua
@@ -84,6 +84,7 @@ vim.o.showmode = false -- Don't show "INSERT" below. It's in the statusline
 vim.o.inccommand = 'split' -- Preview replacements in a split!
 vim.o.foldmethod = 'syntax' -- treesitter needs expr folding, but should be like syntax
 vim.opt.list = true -- show whitespace
+vim.opt.ignorecase = true -- Needed so that smartcase works. Also what one would expect
 vim.opt.smartcase = true -- just :h it. Loupe was setting this before
 
 vim.opt.listchars = { -- Fails if using vim.o, I think vim.opt is better overall
