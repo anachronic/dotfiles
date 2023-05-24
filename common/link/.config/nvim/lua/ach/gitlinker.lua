c936c67148f88472f275ef22c7e156782f3341a0
Nicolás Salas <nsalas@fastmail.com>
2023-05-23 23:03:51 -0400

    neovim: add Permalink(!) command to browse remote
    Feels like this could be accomplished with fugitive, but idk

--- common/link/.config/nvim/lua/ach/gitlinker.lua
+++ common/link/.config/nvim/lua/ach/gitlinker.lua
@@ -1,4 +1,5 @@
 local linker = require('gitlinker')
+local actions = require('gitlinker.actions')
 local ok, private = pcall(require, 'ach.private.gitlinker')
 
 local callbacks = {}
@@ -8,4 +9,18 @@ end
 
 linker.setup({
     callbacks = callbacks,
+    mappings = nil,
+    opts = {
+        print_url = false,
+    },
 })
+
+vim.api.nvim_create_user_command('Permalink', function(opts)
+    local mode = vim.api.nvim_get_mode().mode
+
+    local callback_opts = {}
+    if opts.bang then
+        callback_opts['action_callback'] = actions.open_in_browser
+    end
+    linker.get_buf_range_url(mode, callback_opts)
+end, { bang = true, range = true })
