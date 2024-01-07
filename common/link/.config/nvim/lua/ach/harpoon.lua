0c628ad74ade2466e0d8731c7fb745950d845247
Nicolás Salas <nsalas@fastmail.com>
2024-01-07 15:31:32 -0300

    neovim: delete harpoon completely

--- common/link/.config/nvim/lua/ach/harpoon.lua
+++
@@ -1,8 +0,0 @@
-local telescope = require('telescope')
-
-vim.keymap.set('n', '<leader>m', "<Cmd>lua require('harpoon.mark').add_file()<CR>")
-vim.keymap.set('n', '[g', "<Cmd>lua require('harpoon.ui').nav_next()<CR>")
-vim.keymap.set('n', ']g', "<Cmd>lua require('harpoon.ui').nav_prev()<CR>")
-vim.keymap.set('n', '<leader><leader>', '<Cmd>Telescope harpoon marks<CR>')
-
-telescope.load_extension('harpoon')
