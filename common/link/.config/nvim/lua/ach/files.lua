98949f704989fbdc1f85249f647e175b1e3c3387
Nicolás Salas <nsalas@fastmail.com>
2025-06-04 11:43:46 -0400

    neovim: control own mappings for oil.nvim

--- common/link/.config/nvim/lua/ach/files.lua
+++ common/link/.config/nvim/lua/ach/files.lua
@@ -16,7 +16,27 @@ require('nvim-tree').setup({
     },
 })
 
-require('oil').setup()
+require('oil').setup({
+    use_default_keymaps = false,
+    keymaps = {
+        ["g?"] = { "actions.show_help", mode = "n" },
+        ["<CR>"] = "actions.select",
+        ["<C-s>"] = { "actions.select", opts = { vertical = true } },
+        ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
+        ["<C-t>"] = { "actions.select", opts = { tab = true } },
+        ["<C-p>"] = "actions.preview",
+        ["<C-c>"] = { "actions.close", mode = "n" },
+        ["gr"] = "actions.refresh",
+        ["-"] = { "actions.parent", mode = "n" },
+        ["_"] = { "actions.open_cwd", mode = "n" },
+        ["`"] = { "actions.cd", mode = "n" },
+        ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+        ["gs"] = { "actions.change_sort", mode = "n" },
+        ["gx"] = "actions.open_external",
+        ["g."] = { "actions.toggle_hidden", mode = "n" },
+        ["g\\"] = { "actions.toggle_trash", mode = "n" },
+    },
+})
 
 
 vim.keymap.set('n', '<A-1>', '<cmd>NvimTreeToggle<CR>')
