d685046d45d26b91dd8788bb8b1c6f0a5e0119ec
Nicolás Salas <nsalas@fastmail.com>
2025-06-16 00:12:15 -0400

    neovim: follow stylua
    god, pipelines are annoying lol

--- common/link/.config/nvim/lua/ach/files.lua
+++ common/link/.config/nvim/lua/ach/files.lua
@@ -19,23 +19,22 @@ require('nvim-tree').setup({
 require('oil').setup({
     use_default_keymaps = false,
     keymaps = {
-        ["g?"] = { "actions.show_help", mode = "n" },
-        ["<CR>"] = "actions.select",
-        ["<C-s>"] = { "actions.select", opts = { vertical = true } },
-        ["<C-t>"] = { "actions.select", opts = { tab = true } },
-        ["<C-c>"] = { "actions.close", mode = "n" },
-        ["gr"] = "actions.refresh",
-        ["-"] = { "actions.parent", mode = "n" },
-        ["_"] = { "actions.open_cwd", mode = "n" },
-        ["`"] = { "actions.cd", mode = "n" },
-        ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
-        ["gs"] = { "actions.change_sort", mode = "n" },
-        ["gx"] = "actions.open_external",
-        ["g."] = { "actions.toggle_hidden", mode = "n" },
-        ["g\\"] = { "actions.toggle_trash", mode = "n" },
+        ['g?'] = { 'actions.show_help', mode = 'n' },
+        ['<CR>'] = 'actions.select',
+        ['<C-s>'] = { 'actions.select', opts = { vertical = true } },
+        ['<C-t>'] = { 'actions.select', opts = { tab = true } },
+        ['<C-c>'] = { 'actions.close', mode = 'n' },
+        ['gr'] = 'actions.refresh',
+        ['-'] = { 'actions.parent', mode = 'n' },
+        ['_'] = { 'actions.open_cwd', mode = 'n' },
+        ['`'] = { 'actions.cd', mode = 'n' },
+        ['~'] = { 'actions.cd', opts = { scope = 'tab' }, mode = 'n' },
+        ['gs'] = { 'actions.change_sort', mode = 'n' },
+        ['gx'] = 'actions.open_external',
+        ['g.'] = { 'actions.toggle_hidden', mode = 'n' },
+        ['g\\'] = { 'actions.toggle_trash', mode = 'n' },
     },
 })
-
 
 vim.keymap.set('n', '<A-1>', '<cmd>NvimTreeToggle<CR>')
 vim.keymap.set('n', '<leader>f', '<cmd>NvimTreeToggle<CR>')
